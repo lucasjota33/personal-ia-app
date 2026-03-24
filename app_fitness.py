8228e967b188f8749e34b935cbfce7acd8354984
lucasjota33 <lucasjota33@gmail.com>
2026-03-24 14:13:46 -0300

    mudei o modelo do gemini

--- app_fitness.py
+++ app_fitness.py
@@ -12,7 +12,7 @@ from fpdf import FPDF
 
 # Configurações iniciais
 CHAVE = st.secrets["GEMINI_API_KEY"]
-MODELO = "models/gemini-2.5-flash"
+MODELO = "models/gemini-1.5-flash-8b"
 
 # ==========================================================
 # 🟢 MOTOR DE BANCO DE DADOS EM NUVEM (FIREBASE FIRESTORE)
