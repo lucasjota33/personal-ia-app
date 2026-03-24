cdc0f8f6544d71102f021650d7bba4d5d5580023
lucasjota33 <lucasjota33@gmail.com>
2026-03-24 14:17:34 -0300

    2.0 flash

--- app_fitness.py
+++ app_fitness.py
@@ -12,7 +12,7 @@ from fpdf import FPDF
 
 # Configurações iniciais
 CHAVE = st.secrets["GEMINI_API_KEY"]
-MODELO = "models/gemini-3.1-flash-lite"
+MODELO = "models/gemini-2.0-flash"
 
 # ==========================================================
 # 🟢 MOTOR DE BANCO DE DADOS EM NUVEM (FIREBASE FIRESTORE)
